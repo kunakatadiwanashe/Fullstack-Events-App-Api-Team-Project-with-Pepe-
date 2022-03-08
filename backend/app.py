11eb3083bff57b060234ccc67460902ad0744c4e
Precell <82501476+Precell@users.noreply.github.com>
2022-03-08 11:56:24 +0200

    first commit

--- backend/app.py
+++ backend/app.py
@@ -1,2 +1,13 @@
 from flask import Flask
+from flask_restful import Api
+from routes import Event
+
+BASE_URL = '/api/events'
+
+
 app = Flask(__name__)
+
+api = Api(app)
+
+if __name__ == '__main__':
+    app.run(debug=True)
