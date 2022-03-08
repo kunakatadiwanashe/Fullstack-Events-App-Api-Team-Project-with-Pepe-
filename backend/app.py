d33abb6d76c2d6ac399ff14565734b12c4d8adef
Precell <82501476+Precell@users.noreply.github.com>
2022-03-08 14:05:13 +0200

    pull

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_restful import Api
-from routes import Event
+from Routes import EventsList, Event
 
 BASE_URL = '/api/events'
 
--- backend/routes.py
+++ backend/routes.py
@@ -1,5 +1,24 @@
+import resource
 from flask_restful import Resource
 
-class Event(Resource):
+
+
+class EventsList(resource):
     def get(self):
-        return {'Event!':'This is event 1'}
+        return {'hello': 'from EventsList'}
+
+class Event(resource):
+    def get(self):
+        return {'hello': 'from Event'}
+
+
+class ReviewList(resource):
+    def get(self):
+        return {'hello': 'from reviews'}
+
+
+class Review(resource):
+    def get(self):
+        return {'hello': 'from review'}
+
+
