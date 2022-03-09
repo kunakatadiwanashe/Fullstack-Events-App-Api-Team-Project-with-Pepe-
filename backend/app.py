d044a3652c1abcc4172f455f4be4db73f8fe7d91
Tanya <tanya@uncommon.org>
2022-03-09 11:56:19 +0200

    app.py changes

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,16 @@
 from flask import Flask
-from flask_CORS import CORS
+from flask_restful import Api
+from routes import ReviewList, Event, EventsList, Review
+
 app = Flask(__name__)
+api = Api(app)
 
 BASE_URL = '/events/api'
+
+api.add_resource(EventList, f'{BASE_URL}/EventList')
+api.add_resource(Event, f'{BASE_URL}/Event/<event_id>')
+api.add_resource(ReviewList, f'{BASE_URL}/ReviewList')
+api.add_resource(Review, f'{BASE_URL}/Review/<review_id>')
 
 @app.route('/')
 def team_python():
