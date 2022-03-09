cffb047ce0c286f1dad77f8707cfe4d9adae1a15
Precell <82501476+Precell@users.noreply.github.com>
2022-03-09 13:26:54 +0200

    returned data from repo in routes

--- backend/repository.py
+++ backend/repository.py
@@ -1,8 +1,5 @@
-from backend.models import ReviewModel
-from repository import Repository
-from routes import EventList, Event
 from unittest.mock import MagicMock
-from models import EventModel,ReviewModel
+from models import EventModel, ReviewModel
 
 
 event1 = EventModel("church service","Happening at goromonzi","@4pm", 1)
@@ -13,12 +10,12 @@ review2 = ReviewModel("reviewer by Tine","02-03-2022", 2)
 
 
 class Repository():
-    def events_get_all(self):
+    def events_get_all():
         return[event1,event2]
 
-    def get_event_by_id(self, event_id):
+    def get_event_by_id(event_id):
         events = [event1, event2]
-        return [x for x in events if x.eventId == event_id]
+        return [x.__dict__ for x in events if x.id == event_id]
 
     def review_add(self, data):
         return ReviewModel(data['content'], data['eventId'], 1)
--- backend/routes.py
+++ backend/routes.py
@@ -1,18 +1,20 @@
 from flask_restful import Resource
+from repository import Repository
+
+repo = Repository
 
 class EventsList(Resource):
     def get(self):
-        return {'hello': 'from EventsList'}
+        return [event.__dict__ for event in repo.events_get_all()]
 
 class Event(Resource):
-    def get(self):
-        return {'hello': 'from Event'}
+    def get(self, event_id):
+        return repo.get_event_by_id(int(event_id))
 
 
 class ReviewList(Resource):
     def get(self):
         return {'hello': 'from reviews'}
-
 
 class Review(Resource):
     def get(self):
