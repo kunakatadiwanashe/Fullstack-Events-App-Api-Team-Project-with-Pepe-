b2ce529332a5da0ef44f6f9160c2cb554f0b5bc3
Tanya <tanya@uncommon.org>
2022-03-09 11:34:14 +0200

    new change

--- backend/routes.py
+++ backend/routes.py
@@ -1,21 +1,24 @@
+import resource
 from flask_restful import Resource
 
-class EventsList(Resource):
+
+
+class EventsList(resource):
     def get(self):
         return {'hello': 'from EventsList'}
 
-class Event(Resource):
-    def get(self):
-        return {'hello': 'from Event'}
+class Event(resource):
+    def get(self, event_id):):
+        return {'hello': 'from Event {event_id}'}
 
 
-class ReviewList(Resource):
+class ReviewList(resource):
     def get(self):
         return {'hello': 'from reviews'}
 
 
-class Review(Resource):
-    def get(self):
-        return {'hello': 'from review'}
+class Review(resource):
+    def get(self, review_id)):
+        return {'hello': 'from review {review_id}'}
 
 
