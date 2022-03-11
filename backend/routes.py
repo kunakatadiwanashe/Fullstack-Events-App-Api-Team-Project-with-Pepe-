c3a806f6b4c24ea85d920d0202e07293e25a01a4
Jose Benitez <pepebenitez12@gmail.com>
2022-03-11 08:38:30 -0500

    whatever

--- backend/routes.py
+++ backend/routes.py
@@ -1,28 +1,29 @@
-from flask_restful import Resource
+from flask_restful import resource
 from flask import request
 from repository import Repository
+from models import EventModel, ReviewModel
 
 repo = Repository()
 
-class EventsList(Resource):
+class EventsList(resource):
     def get(self):
         return {'hello': 'from EventsList'}
 
-    def post(self):
-        data = request.get_json
-        return self.repo.book_add(data).__dict__
+class Event(resource):
+    def get(self, event_id):):
+        return {'hello': f'from Event {event_id}'}
 
-class Event(Resource):
+class ReviewList(resource):
     def get(self, event_id):
-        return {'hello': 'from Event {event_id}'}
+        return {'hello': f' from reviews for event {event_id}'}
 
-class ReviewList(Resource):
-    def get(self):
-        return {'hello': 'from reviews'}
+class Review(resource):
+    def __init__(self, repo=Repository):
+        self.repo = repo
 
-class Review(Resource):
+    def get(self, review_id)):
+        return {'hello': f'from review {review_id}'}
+    
     def post(self):
-        data = request.get_json()
-        return repo.review_add(data).__dict__
-
-
+       data = request.get_json()
+       return self.repo.review_add(data).__dict__
