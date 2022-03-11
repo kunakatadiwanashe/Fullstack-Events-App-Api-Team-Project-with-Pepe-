04458debcb550c070300243d8f2625791ee6a6a3
Precell <82501476+Precell@users.noreply.github.com>
2022-03-11 15:26:19 +0200

    fixing

--- backend/routes.py
+++ backend/routes.py
@@ -1,31 +1,23 @@
+import resource
 from flask_restful import Resource
-from repository import Repository
 
-repository = Repository
 
-class EventsList(Resource):
-    def __init__(self, repo=repository):
-        self.repo = repo
-        
+
+class EventsList(resource):
     def get(self):
-        return [event.__dict__ for event in self.repo.events_get_all()]
+        return {'hello': 'from EventsList'}
 
-class Event(Resource):
-    def __init__(self, repo=repository):
-        self.repo = repo
-        
-    def get(self, event_id):
-        return self.repo.get_event_by_id(int(event_id))
-
-
-class ReviewList(Resource):
-    def __init__(self, repo=repository):
-        self.repo = repo
-        
+class Event(resource):
     def get(self):
-        return [review.__dict__ for review in self.repo.reviews_get_all()]
+        return {'hello': 'from Event'}
 
-class Review(Resource):
+
+class ReviewList(resource):
+    def get(self):
+        return {'hello': 'from reviews'}
+
+
+class Review(resource):
     def get(self):
         return {'hello': 'from review'}
 
