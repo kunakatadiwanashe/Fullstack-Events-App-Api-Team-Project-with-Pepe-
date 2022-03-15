f85b85037aa77e84705f97123e3c6448f905cd9b
Jose Benitez <pepebenitez12@gmail.com>
2022-03-15 07:15:19 -0500

    Changed routes to lowercase r

--- backend/app.py
+++ backend/app.py
@@ -7,10 +7,10 @@ api = Api(app)
 
 BASE_URL = '/events/api'
 
-api.add_resource(EventsList, f'{BASE_URL}/Events')
-api.add_resource(Event, f'{BASE_URL}/Event/<event_id>')
-api.add_resource(ReviewList, f'{BASE_URL}/Reviews')
-api.add_resource(Review, f'{BASE_URL}/Review')
+api.add_resource(EventsList, f'{BASE_URL}/events')
+api.add_resource(Event, f'{BASE_URL}/event/<event_id>')
+api.add_resource(ReviewList, f'{BASE_URL}/reviews')
+api.add_resource(Review, f'{BASE_URL}/review')
 
 if __name__ == '__main__':
     app.run(debug=True)
